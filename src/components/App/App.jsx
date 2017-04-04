734a992745e706f5182fab644ef94ace8dea688e
Lazy Code <fbenalex@gmail.com>
2017-04-04 17:54:59 +0300

    server routing

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import Grid  from 'react-bootstrap/lib/Grid';
 import Nav from 'react-bootstrap/lib/Nav';
 import Navbar from 'react-bootstrap/lib/Navbar';
 import NavItem  from 'react-bootstrap/lib/NavItem';
-import HelloWorldPage from 'components/HelloWorldPage';
+import { Link } from 'react-router';
+import LinkContainer from 'react-router-bootstrap/lib/LinkContainer';
 import './bootstrap.css';
+
+const propTypes = {
+  children: PropTypes.node
+};
 
 class App extends Component {
   render() {
@@ -13,23 +18,29 @@ class App extends Component {
         <Navbar>
           <Navbar.Header>
             <Navbar.Brand>
-              <span>Hello World</span>
+              <Link to='/'>Hello World</Link>
             </Navbar.Brand>
             <Navbar.Toggle />
           </Navbar.Header>
           <Navbar.Collapse>
             <Nav navbar>
-              <NavItem>Время</NavItem>
-              <NavItem>Счетчики</NavItem>
+              <LinkContainer to='/time'>
+                <NavItem>Время</NavItem>
+              </LinkContainer>
+              <LinkContainer to='/counters'>
+                <NavItem>Счетчики</NavItem>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
         <Grid>
-          <HelloWorldPage />
+          {this.props.children}
         </Grid>
       </div>
     );
   }
 }
+
+App.propTypes = propTypes;
 
 export default App;
--- src/server.js
+++ src/server.js
@@ -1,14 +1,29 @@
 import express from 'express';
 import React from 'react';
 import ReactDom from 'react-dom/server';
-import App from 'components/App';
+import { match, RouterContext } from 'react-router';
+import routes from './routes';
 
 const app = express();
 
 app.use((req, res) => {
-  const componentHTML = ReactDom.renderToString(<App />);
+  match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
+    if (redirectLocation) {
+      return res.redirect(301, redirectLocation.pathname + redirectLocation.search);
+    }
 
-  return res.end(renderHTML(componentHTML));
+    if (error) {
+      return res.status(500).send(error.message);
+    }
+
+    if (!renderProps) {
+      return res.status(404).send('Not found');
+    }
+
+    const componentHTML = ReactDom.renderToString(<RouterContext {...renderProps} />);
+
+    return res.end(renderHTML(componentHTML));
+  });
 });
 
 const assetUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:8050' : '/';
@@ -24,7 +39,7 @@ function renderHTML(componentHTML) {
           <link rel="stylesheet" href="${assetUrl}/public/assets/styles.css">
       </head>
       <body>
-        <div id="react-view">${componentHTML}</div>
+        <div id="react-view"><div>${componentHTML}</div></div>
         <script type="application/javascript" src="${assetUrl}/public/assets/bundle.js"></script>
       </body>
     </html>
